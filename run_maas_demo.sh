eede4a084facd33cfac7eeef7f2bc15421ae2a8a
HeJia <nicehejia@gmail.com>
2026-02-25 16:52:57 +0800

    fix git

--- run_maas_demo.sh
+++ run_maas_demo.sh
@@ -1,13 +1,13 @@
-#ais_bench --models maas --datasets gsm8k_gen_0_shot_noncot_chat_prompt --debug
+#!/usr/bin/env bash
+# 加载 .env（export 给 ais_bench 子进程，让 maas.py 里的 os.environ 能读到）
+set -a; source "$(dirname "$0")/.env"; set +a
+
 
 # ais_bench \
 #     --models maas \
 #     --custom-dataset-path /path/to/your_data.jsonl \
 #     --custom-dataset-data-type qa \
 #     --mode all
-
-
-# ais_bench --models maas --datasets custom_eval_suite --debug
 
 
 ais_bench --models maas --datasets task_38_suite --debug
